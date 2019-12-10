033b96bd0036d5057d31cfa9681529891de0abd1
EdwardFriend <luzardoeduardo2000@gmail.com>
2019-12-10 13:02:23 -0500

    casi terminado

--- src/clases/Rectangulo.java
+++ src/clases/Rectangulo.java
@@ -1,10 +1,19 @@
 package clases;
 import java.util.Scanner;
+/**
+ * Esta es la clase Rectangulo en la que se instancia la clase Coordenada
+ * @author MARIO LUZARDO
+ *
+ */
 public class Rectangulo{
 	
-	Coordenada vector []=new Coordenada[4];
+	Coordenada vector []=new Coordenada[4];/*se instancia la clase Coordenada con un vector que nos
+	                                        *permitira manejar bien los vertices para crear un rectangulo*/
 
 	public Rectangulo() {
+		/*se crea un constructor vacio en el que crearemos los rectangulos
+		 * partiendo de sus vertices
+		 */
 		Scanner in = new Scanner(System.in);
 		int x=0;
 		int y=0;
@@ -18,6 +27,9 @@ public class Rectangulo{
 	}
 
 	public Rectangulo(int base, int altura) {
+		/*se crea un constructor parametrico para formar el rectangulo
+		 *usando la base y altura del mismo
+		 */
 		for(int i=0;i<vector.length;i++) {
 			vector[i]=new Coordenada();
 		}
@@ -27,11 +39,17 @@ public class Rectangulo{
 		vector[3].desplazarArriba(altura);
 	}
 	public void mostrar() {
+		/*el metodo mostrar nos permite visualizar los pares ordenados que
+		 * conforman los vertices de nuestro rectangulo
+		 */
 		for(int i=0;i<vector.length;i++) {
 			vector[i].mostrar();
 		}
 	}
 	public void validacion() {
+		/*el metodo validacion nos permite analizar que los valores ingresados para cada par ordenado
+		 * formen realmente un rectangulo, de otro modo estos se encuentran formando cualquier otro poligono
+		 */
 		if((vector[2].getX().getPuntob())==(vector[3].getX().getPuntob())) {
 			if((vector[1].getX().getPuntoa())==(vector[3].getX().getPuntoa())) {
 				if((vector[0].getX().getPuntoa())==(vector[2].getX().getPuntoa())) {
@@ -60,27 +78,33 @@ public class Rectangulo{
 		}
 	}
 	public void calcularArea() {
+		/*este metodo nos permite calcular el area del rectangulo que hemos creado*/
 		System.out.println((vector[1].getX().getPuntoa())*vector[3].getX().getPuntob());
 	}
 	public void calcularPerimetro() {
+		/*este metodo nos permite calcular el perimetro del rectangulo que hemos creado*/
 		System.out.println(2*(vector[1].getX().getPuntoa())+2*(vector[3].getX().getPuntob()));
 	}
 	public void desplazarRectanguloderecha(int a) {
+		/*con este metodo podemos desplazar el rectangulo a la derecha en el plano cartesiano*/
 		for(int i=0;i<vector.length;i++) {
 			vector[i].desplazarDerecha(a);
 		}
 	}
 	public void desplazarRectanguloizquierda(int a) {
+		/*con este metodo podemos desplazar el rectangulo a la izquierda en el plano cartesiano*/
 		for(int i=0;i<vector.length;i++) {
 			vector[i].desplazarIzquierda(a);
 		}
 	}
 	public void desplazarRectanguloarriba(int a) {
+		/*con este metodo podemos desplazar el rectangulo hacia arriba en el plano cartesiano*/
 		for(int i=0;i<vector.length;i++) {
 			vector[i].desplazarArriba(a);
 		}
 	}
 	public void desplazarRectanguloabajo(int a) {
+		/*con este metodo podemos desplazar el rectangulo hacia abajo en el plano cartesiano*/
 		for(int i=0;i<vector.length;i++) {
 			vector[i].desplazarAbajo(a);
 		}
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -4,17 +4,19 @@
 package principal;
 import clases.Rectangulo;
 /**
- * @author ALICIA FIERRO
+ * Esta es la clase principal
+ * @author MARIO LUZARDO
  *
  */
 public class Principal {
 
 	/**
+	 * Este es el metodo main
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Rectangulo rec = new Rectangulo(5,4);
+		Rectangulo rec = new Rectangulo(5,4);/**aqui instanciamos la clase Rectangulo*/
 		rec.mostrar();
 		rec.validacion();
 		rec.calcularArea();
@@ -22,8 +24,6 @@ public class Principal {
 		rec.desplazarRectanguloarriba(1);
 		rec.desplazarRectanguloderecha(1);
 		rec.mostrar();
-		//Rectangulo rec2 = new Rectangulo();
-		//rec2.mostrar();
-		//rec2.validacion();
+
 	}
 }
